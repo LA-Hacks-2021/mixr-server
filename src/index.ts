be6d08ac1a1726efabc1f72be4dd37df96954bb9
Charles Liu <liuchangyang112@gmail.com>
2021-03-23 22:45:47 +0800

    Add twilio token generation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,43 @@
 import express from "express";
+import Twilio from "twilio";
+
+import {
+  PORT,
+  TWILIO_ACCOUNT_SID,
+  TWILIO_API_KEY,
+  TWILIO_API_SECRET,
+} from "./config.js";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const AccessToken = Twilio.jwt.AccessToken;
+const ChatGrant = AccessToken.ChatGrant;
 
-app.get("/", (_, res) => {
-  res.status(200).json({
-    message: "good day",
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/**
+ * Token generation request
+ */
+app.get("/token/:identity", (req, res) => {
+  const identity = req.params.identity;
+  const token = new AccessToken(
+    TWILIO_ACCOUNT_SID,
+    TWILIO_API_KEY,
+    TWILIO_API_SECRET
+  );
+
+  token.identity = identity;
+  token.addGrant(
+    new ChatGrant({
+      serviceSid: process.env.TWILIO_CHAT_SERVICE_SID,
+    })
+  );
+
+  res.send({
+    identity: token.identity,
+    jwt: token.toJwt(),
   });
 });
 
+const port = PORT || 5000;
 app.listen(port, () => console.log(`Running on port ${port}`));
